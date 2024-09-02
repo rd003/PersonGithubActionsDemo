77fc443f61caa721e8eb4594af6a874484101d3c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-09-02 13:40:37 +0530

    feat: people controller get person  method

--- PersonGithubActionsDemo.Api/Controllers/PeopleApiController.cs
+++ PersonGithubActionsDemo.Api/Controllers/PeopleApiController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PersonGithubActionsDemo.Api.DTOS;
 using PersonGithubActionsDemo.Api.Extensions;
 using PersonGithubActionsDemo.Api.Services;
 
@@ -24,6 +25,25 @@ public class PeopleController : ControllerBase
         {
             var people = (await _personService.GetPeopleAsync()).Select(p => p.ToPersonReadDto()).ToList();
             return Ok(people);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpGet("{id}", Name = "GetPerson")]
+    public async Task<IActionResult> GetPerson(int id)
+    {
+        try
+        {
+            PersonReadDTO? person = (await _personService.GetPersonAsync(id))?.ToPersonReadDto();
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
         }
         catch (Exception ex)
         {
